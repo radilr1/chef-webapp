8a301b0bd32ef1bee70349fec9f04350d0f2b699
Radil Radenkov <radil@buzzstarter.com>
2015-11-20 17:16:47 +0200

    version bump

--- metadata.rb
+++ metadata.rb
@@ -15,6 +15,6 @@ depends 'redisio', '~> 1.7.0'
 
 # Note: These deps come from Github, and must be imported via your Cheffile (versions fixed to
 # ensure Github version is used or a warning is thrown)
-depends 'rvm', '~> 0.9.1' # must use version from github: 'fnichol/chef-rvm'
+depends 'rvm', '~> 0.10.1' # must use version from github: 'fnichol/chef-rvm'
 depends 'monit', '~> 1.5.0' # must use version from github: 'phlipper/chef-monit'
 depends 'unicorn', '~> 1.3.1' # must use version from github: 'AmoebaLabs/chef-unicorn'
