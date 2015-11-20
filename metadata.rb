ba906fa760351b39b59fe65a6a7d96ef0f9c86bf
Radil Radenkov <radil@buzzstarter.com>
2015-11-20 17:47:11 +0200

    revert

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "sayhi@amoe.ba"
 license          "MIT"
 description      "Deploy a web application (Ruby on Rails or NodeJS) using industry best practices. Used with Amoeba Deploy Tools."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.1.8"
 
 recipe           "webapp", "Default webapp recipe"
 
@@ -15,6 +15,6 @@ depends 'redisio', '~> 1.7.0'
 
 # Note: These deps come from Github, and must be imported via your Cheffile (versions fixed to
 # ensure Github version is used or a warning is thrown)
-depends 'rvm', '~> 0.10.1' # must use version from github: 'fnichol/chef-rvm'
+depends 'rvm', '~> 0.9.1' # must use version from github: 'fnichol/chef-rvm'
 depends 'monit', '~> 1.5.0' # must use version from github: 'phlipper/chef-monit'
 depends 'unicorn', '~> 1.3.1' # must use version from github: 'AmoebaLabs/chef-unicorn'
